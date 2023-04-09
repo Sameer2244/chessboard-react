7fa38d3c9a217f785821a2bf5b33b4a30fd643a7
Sameer2244 <31188576+Sameer2244@users.noreply.github.com>
2023-04-09 16:23:31 +0530

    3 files modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
+import { board } from './board/board';
 
 export default function App() {
-  return <div></div>;
+  const [fields, setFields] = useState(board);
+
+  const changeColor = (row, column) => {
+    console.log(row, column);
+  };
+  return (
+    <div className="field-container">
+      {fields.map((e, i) => {
+        return e.map((e1, i1) => {
+          return (
+            <div
+              className="field"
+              key={i + i1}
+              style={{ background: `${e1}` }}
+              onClick={() => {
+                changeColor(i, i1);
+              }}
+            ></div>
+          );
+        });
+      })}
+    </div>
+  );
 }
